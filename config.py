f5001fcd324a75eefaa8afe6b15b5ff80cb3d50b
kivanets <kivanets@k1v4nets.local>
2022-12-04 21:00:27 +0200

    remove unnecessary print, afterthought test_out_of_range

---
+++ config.py
@@ -0,0 +1,3 @@
+TEST_DATA_NAMES = ['Byron Fields', 'Charles Morris', 'Emma Wong', 'Eve Holt', 'George Bluth', 'George Edwards',
+                   'Janet Weaver', 'Lindsay Ferguson', 'Michael Lawson', 'Rachel Howell', 'Tobias Funke',
+                   'Tracey Ramos']
